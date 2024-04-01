c401e1c04c4aea8cd2036c5c1b04aa877992f7d2
AndreyTerechenko <Dronka97@yandex.ru>
2024-04-01 21:08:13 +0300

    homeWork9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+        double sum = 0;
+        double[] expenses = {20000, 30000, 12000, 17000, 11000};
+        for (int i = 0; i < expenses.length; i++) {
+            sum = sum + expenses[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + (long) sum + " рублей");
+        // Task 2
+        double minSum = expenses[0];
+        double maxSum = minSum;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxSum) {
+                maxSum = expenses[i];
+            }
+            if (minSum > expenses[i]) {
+                minSum = expenses[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + (long) maxSum + " рублей. " +
+                "\nМинимальная сумма трат за неделю составила " + (long) minSum + " рублей.");
+        // Task 3
+        double averageExpenses = sum / expenses.length;
+        System.out.println("Средняя сумма трат за месяц составила " + (long) averageExpenses + " рублей");
+        // Task 4
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 10; reverseFullName.length > i; i--) {
+            if (i == -1) {
+                break;
+            }
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
